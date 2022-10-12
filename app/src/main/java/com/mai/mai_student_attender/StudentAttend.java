3e2d8457a0e7c825d28973738befbd893382cd78
unknown <andrey.dubovsky@gmail.com>
2022-10-12 09:32:54 +0300

    Added many empty activities

---
+++ app/src/main/java/com/mai/mai_student_attender/StudentAttend.java
@@ -0,0 +1,14 @@
+package com.mai.mai_student_attender;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudentAttend extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_attend);
+    }
+}
