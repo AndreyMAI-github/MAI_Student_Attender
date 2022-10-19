78f6cc9ac7a43549db76ae6670354bd89315a534
unknown <andrey.dubovsky@gmail.com>
2022-10-19 10:13:33 +0300

    Updated some sdk elements. Fixed build crash

--- app/src/main/java/com/mai/mai_student_attender/MainActivity.java
+++ app/src/main/java/com/mai/mai_student_attender/MainActivity.java
@@ -3,12 +3,52 @@ package com.mai.mai_student_attender;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String tag = "uWu";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(tag, "Created");
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(tag, "Started");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(tag, "Resumed");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(tag, "Paused");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(tag, "Stopped");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(tag, "Restarted");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(tag, "Destroyed");
+    }
+
 }
