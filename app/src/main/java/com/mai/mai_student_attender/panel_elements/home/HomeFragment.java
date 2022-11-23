ab30428759c2c4fffcc86fde9774d78fd5079aaa
AndreyMAI-github <andrey.dubovsky@gmail.com>
2022-11-23 09:36:37 +0300

    Merge with main

--- app/src/main/java/com/mai/mai_student_attender/panel_elements/home/HomeFragment.java
+++ app/src/main/java/com/mai/mai_student_attender/panel_elements/home/HomeFragment.java
@@ -19,9 +19,7 @@ public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        return
-        inflater.inflate(R.layout.fragment_home,null);
-
+        return inflater.inflate(R.layout.fragment_home,null);
     }
 
 }
