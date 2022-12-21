9c458cc798b1285f389c129e17d76637526de884
ilya0003 <ishatohin22@gmail.com>
2022-12-21 09:40:19 +0300

    исправлен фрагмент

--- app/src/main/java/com/mai/mai_student_attender/MainActivity.java
+++ app/src/main/java/com/mai/mai_student_attender/MainActivity.java
@@ -3,26 +3,8 @@ package com.mai.mai_student_attender;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.Toast;
-
-import com.mai.mai_student_attender.panel_elements.UnitAdapter;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button btnCalendar;
-    private final String tag = "uWu";
-
-    private View.OnClickListener btnHandler = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Logger.d("Button btnCalendar clicked");
-            (Toast.makeText(getApplicationContext(),"btnCalendarClicked", Toast.LENGTH_LONG)).show();
-        }
-    };
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,25 +12,10 @@ public class MainActivity extends AppCompatActivity {
         Logger.d("MainActivity Created");
 
         setContentView(R.layout.activity_main);
-        btnCalendar = findViewById(R.id.bActMainCalendar);
-        btnCalendar.setOnClickListener(btnHandler);
-
-        ListView home_list = (ListView) findViewById(R.id.home_list);
-
-        Unit[] units = {
-                new Unit("а", "10", "1"), //добавить фото
-                new Unit("б", "11", "2"),
-                new Unit("в", "12", "2"),
-                new Unit("г", "13", "1")
-        };
-
-        UnitAdapter unitAdapter = new UnitAdapter(this, units);
-        home_list.setAdapter(unitAdapter);
-
 
     }
-
-    @Override
+}
+    /*@Override
     protected void onStart() {
         super.onStart();
         Logger.d("MainActivity Started");
@@ -84,5 +51,5 @@ public class MainActivity extends AppCompatActivity {
         Logger.d("MainActivity Destroyed");
     }
 
-}
+}*/
 
