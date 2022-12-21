98a0bde383975cda87c317260243e039b613cc6e
ilya0003 <ishatohin22@gmail.com>
2022-12-21 09:41:57 +0300

    исправлен фрагмент

--- app/src/main/java/com/mai/mai_student_attender/panel_elements/home/HomeFragment.java
+++ app/src/main/java/com/mai/mai_student_attender/panel_elements/home/HomeFragment.java
@@ -29,7 +29,7 @@ public class HomeFragment extends Fragment {
     private FragmentDisciplineBinding binding;
 
     @SuppressLint("MissingInflatedId")
-    @Override
+    @Override //
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home,
